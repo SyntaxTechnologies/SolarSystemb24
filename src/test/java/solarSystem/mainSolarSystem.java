3f18a414db3751ee597562be50e1ddaa785d628f
SyntaxTechnologies <moazzam@syntaxtechs.com>
2026-01-06 20:19:03 -0500

    i am done with planet2

--- src/test/java/solarSystem/mainSolarSystem.java
+++ src/test/java/solarSystem/mainSolarSystem.java
@@ -14,5 +14,11 @@ public class mainSolarSystem {
         sunEx.temp="90k";
 
 
+        planet2 peterPan= new planet2();
+        peterPan.size="4545454";
+        peterPan.color="green";
+
+
+
     }
 }
